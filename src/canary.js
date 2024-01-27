b4b24cf0cd0e7562099af6d5b79c7dd4a6d85c49
Tolga1452 <tolgchu-studios@outlook.com>
2024-01-27 15:07:29 +0300

    also create other folders

--- src/canary.js
+++ src/canary.js
@@ -2,4 +2,6 @@ const { mkdirSync, existsSync } = require("fs");
 const { _getDiscordClientData } = require("./");
 
 if (!existsSync("./save")) mkdirSync("./save");
+if (!existsSync("./save/scripts")) mkdirSync("./save/scripts");
+if (!existsSync("./save/styles")) mkdirSync("./save/styles");
 _getDiscordClientData("discordClientCanary");
